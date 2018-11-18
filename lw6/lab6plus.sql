06ee1e9c479d379a2421688da11a8df588640c3b
elena-zaharova <elenazahar1998@gmail.com>
2018-11-18 21:34:07 +0300

    Update lw6/lab6plus.sql

--- lw6/lab6plus.sql
+++ lw6/lab6plus.sql
@@ -24,29 +24,53 @@ CREATE TABLE track_downloads (
        
       PRIMARY KEY (download_id) 
     ); 
--- Напишите SQL-запрос, возвращающий все пары (download_count, user_count), 
---    удовлетворяющие следующему условию: 
---    user_count — общее ненулевое число пользователей, сделавших 
---    ровно download_count скачиваний 19 ноября 2010 года.
-SELECT download_count, user_count FROM 
 
 
--- 4  Необходимо создать таблицу студентов (поля id, name) и таблицу курсов (поля id, name). Каждый студент может посещать несколько курсов. Названия курсов и имена студентов - произвольны.
+-- 4  Необходимо создать таблицу студентов (поля id, name)
+-- и таблицу курсов (поля id, name).
+-- Каждый студент может посещать несколько курсов. Названия курсов и имена студентов - произвольны.
 -- Написать SQL запросы: 
---    1. отобразить количество курсов, на которые ходит более 5 студентов
+CREATE TABLE course ( 
+      course_id BIGINT(20) NOT NULL AUTO_INCREMENT, 
+      name VARCHAR(50), 
+      PRIMARY KEY (course_id) 
+    ); 
+    CREATE TABLE students ( 
+	  students_id BIGINT(20) NOT NULL AUTO_INCREMENT, 
+      name VARCHAR(50),        
+      PRIMARY KEY (download_id) 
+    ); 
+ CREATE TABLE students_x_courses ( 
+	  students_x_courses_id BIGINT(20) NOT NULL AUTO_INCREMENT, 
+      course_id BIGINT(20) NOT NULL, 
+      students_id BIGINT(20) NOT NULL,
+      PRIMARY KEY (students_x_courses_id) 
+    ); 
 
+--    1. отобразить количество курсов, на которые ходит более 5 студентов
+SELECT count(students_x_courses.course_id) FROM students_x_courses
+WHERE count(students_x_courses.students_id) > 5;
 
 --    2. отобразить все курсы, на которые записан определенный студент.
+SELECT courses.name, students.name 
+FROM students_x_courses
+LEFT JOIN students
+ON students_id.students_x_courses = students_id.students
+LEFT JOIN course
+ON course_id.students_x_courses = course_id.course;
 
+-- 5 Может ли значение в столбце(ах), на который наложено ограничение
+-- foreign key, равняться null? Привидите пример. 
 
+-- Может если на данный столбец не нанесено ограничение NOT NULL, 
+-- например припостроении таблицы дерева файловой системы столбец foreign key 
+-- ссылка на эту же самую таблицу, кортеж с информацией о родительской директории 
+-- тогда для корневой дерктор этой файловой системы в столбце будет null
 
+-- 6 Как удалить повторяющиеся строки с использованием 
+-- ключевого слова Distinct? Приведите пример таблиц с данными и запросы. 
 
--- 5 Может ли значение в столбце(ах), на который наложено ограничение foreign key, равняться null? Привидите пример. 
-
-
--- 6 Как удалить повторяющиеся строки с использованием ключевого слова Distinct? Приведите пример таблиц с данными и запросы. 
-
-
+SELECT DISTINCT name_of_columns FROM name_of_table;
 
 
 -- 7  Есть две таблицы:
@@ -54,8 +78,16 @@ SELECT download_count, user_count FROM
 --    orders - таблица с заказами (orders_id, users_id, status)
 --    1) Выбрать всех пользователей из таблицы users, у которых ВСЕ записи в таблице orders имеют status = 0
 
+SELECT name FROM users
+LEFT JOIN orders
+ON orders.users_id = users.users_id
+WHERE status = 0 ;
 
 --    2) Выбрать всех пользователей из таблицы users, у которых больше 5 записей в таблице orders имеют status = 1
+SELECT name FROM users
+LEFT JOIN orders
+ON orders.users_id = users.users_id
+WHERE status = 1 AND COUNT(orders.users_id) > 5  ;
 
 
 -- 8)  В чем различие между выражениями HAVING и WHERE? 
